2a3b0f3b20fb43cbde27ab4313c654ecefa5f17e
Nadia Cruz <112596017+NadiaCruz1@users.noreply.github.com>
2023-03-08 17:51:04 -0300

    commit2

---
+++ func.js
@@ -0,0 +1,29 @@
+function Pinterest(){
+    var confirmar = confirm('Estas seguro que deseas salir?');
+    if (confirmar){
+      
+      window.location.href = 'https://pin.it/3loVmy1'
+    
+    }else{
+    }
+}
+
+function mail(){
+        var confirmar = confirm('Estas seguro que deseas salir?');
+        if (confirmar){
+          
+          window.location.href = 'mailto:m.bluth@example.com'
+        
+        }else{
+        }
+}
+function phone(){
+            var confirmar = confirm('Estas seguro que deseas salir?');
+            if (confirmar){
+              
+              window.location.href = 'tel:+123456789'
+            
+            }else{
+            }
+}
+            
